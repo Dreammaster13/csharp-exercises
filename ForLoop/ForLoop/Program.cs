6ed2b9851112e7a7159c1258eab17d35493b36e2
Rim Fardiyev <threeacesstl@gmail.com>
2018-11-01 21:01:35 -0400

    For loop (0 to 10)

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main()
+        {
+            //The loop continues running through the body until "i" equals 10
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
