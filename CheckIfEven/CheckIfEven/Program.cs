112c3f9fbd46bbfff740c5aed13f83e65a9f7de7
Rim Fardiyev <threeacesstl@gmail.com>
2018-11-04 00:25:45 -0400

    check if entered number is even or odd example

---
+++ CheckIfEven/CheckIfEven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CheckIfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter the number: ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("The number is even.");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("The number is odd.");
+                Console.Read();
+            }
+        }
+    }
+}
